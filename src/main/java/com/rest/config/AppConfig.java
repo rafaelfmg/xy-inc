7bcc4e41aa110c20d595e0e43c1eadb2f341147e
rafaelfmg <rafaelf.fmg@gmail.com>
2016-12-21 08:45:02 -0200

    ajustes

--- src/main/java/com/rest/config/AppConfig.java
+++ src/main/java/com/rest/config/AppConfig.java
@@ -1,15 +1,12 @@
 package com.rest.config;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
-import com.rest.util.ObjectIdSerializer;
 
 
 @Configuration
@@ -29,10 +26,4 @@ public class AppConfig {
 		return mongoClient.getDatabase("modelsRest");
 	}
 
-	@Bean
-	public Jackson2ObjectMapperBuilder configeJackson() {
-		Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder = new Jackson2ObjectMapperBuilder();
-		jackson2ObjectMapperBuilder.serializerByType(ObjectId.class, new ObjectIdSerializer());
-		return jackson2ObjectMapperBuilder;
-	}
 }
--- src/main/java/com/rest/service/impl/DocumentModelServiceImpl.java
+++ src/main/java/com/rest/service/impl/DocumentModelServiceImpl.java
@@ -11,7 +11,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.rest.service.DocumentModelService;
 import com.rest.util.IFilters;
-import com.rest.util.Validation;
 
 @Service
 public class DocumentModelServiceImpl implements DocumentModelService{
@@ -62,36 +61,15 @@ public class DocumentModelServiceImpl implements DocumentModelService{
 	}
 
 	public Document save(Document doc) {
-		validationModel(doc,"/");
+		
 		collection = mongoDatabase.getCollection("/");
 		collection.insertOne(doc);
 		return doc;
 	}
 
 	public boolean update(String documentID, Document doc) {
-		validationModel(doc,"/");
 		collection = mongoDatabase.getCollection("/");
 		return collection.replaceOne(filters.filterByID(documentID), doc).wasAcknowledged();
-	}
-	
-	private void validationModel(Document doc, String modelName){
-		Document document = findModel(modelName);
-		Validation validation = new Validation(document);
-		
-		validation.validate(doc);
-	}
-	
-	private Document findModel(String collectionName) {
-		if ("/".equals(collectionName)) {
-			return Document.parse(
-					"{\"model\":\"/\",\"fields\":[{\"name\":\"model\",\"type\":\"String\",\"required\":true},{\"name\":\"fields\",\"type\":\"ArrayList\"}]}");
-		}
-
-		Document modelo = mongoDatabase.getCollection("/").find(filters.filterByModel(collectionName)).first();
-		if (modelo == null) {
-			throw new IllegalArgumentException("coleção sem modelo");
-		}
-		return modelo;
 	}
 
 		
